dbccf085a603b60414b6a55274e9fa20f91e0529
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-18 20:31:17 -0300

    wip handleGetAccount

--- api.go
+++ api.go
@@ -9,38 +9,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func writeJSON(w http.ResponseWriter, status int, v any) error {
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.WriteHeader(status)
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	return json.NewEncoder(w).Encode(v)
 }
 
-type apiFunc func(http.ResponseWriter, *http.Request) error
+type APIFunc func(http.ResponseWriter, *http.Request) error
 
-type apiError struct {
+type APIError struct {
 	Error string
 }
 
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+func makeHTTPHandleFunc(f APIFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
-			writeJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
 		}
 	}
 }
 
-type apiServer struct {
+type APIServer struct {
 	listenAddr string
 }
 
-func newAPIServer(listenAddr string) *apiServer {
-	return &apiServer{
+func newAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
 		listenAddr: listenAddr,
 	}
 }
 
-func (s *apiServer) Run() {
+func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccout))
@@ -50,7 +50,7 @@ func (s *apiServer) Run() {
 	http.ListenAndServe(s.listenAddr, router)
 }
 
-func (s *apiServer) handleAccout(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleAccout(w http.ResponseWriter, r *http.Request) error {
 	if r.Method == "GET" {
 		return s.handleGetAccout(w, r)
 	}
@@ -64,18 +64,20 @@ func (s *apiServer) handleAccout(w http.ResponseWriter, r *http.Request) error {
 	return fmt.Errorf("method not allowed %s", r.Method)
 }
 
-func (s *apiServer) handleGetAccout(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleGetAccout(w http.ResponseWriter, r *http.Request) error {
+	account := NewAccount("Eduardo", "Freitas")
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleCreateAccout(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *apiServer) handleCreateAccout(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleDeleteAccout(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (s *apiServer) handleDeleteAccout(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
-func (s *apiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
